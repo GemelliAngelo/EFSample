6ec5d23b27bc4f346d50ac2b1e5791e83b1606ad
Angelo Gemelli <angelo.gemelli@bitspa.it>
2025-10-15 12:24:28 +0200

    add Insert

--- EFSample.App/Program.cs
+++ EFSample.App/Program.cs
@@ -1,20 +1,132 @@
-﻿using Serilog;
+﻿using EFSample.Domain.Entities;
+using EFSample.Infrastructure;
+using Microsoft.EntityFrameworkCore;
+using Serilog;
 using System.Reflection;
+using System.Text.Json;
+using System.Text.Json.Serialization;
 
 namespace EFSample.App
 {
     internal class Program
     {
+        private static readonly JsonSerializerOptions _options = new() { WriteIndented = true, ReferenceHandler = ReferenceHandler.IgnoreCycles };
+
         static void Main(string[] args)
         {
             Log.Logger = new LoggerConfiguration()
-               .MinimumLevel.Debug()
-               .WriteTo.Console()
-               .CreateLogger();
+                .MinimumLevel.Verbose()
+                .WriteTo.Console()
+                .CreateLogger();
 
             try
             {
                 Log.Information($"App info: {Assembly.GetExecutingAssembly().FullName}");
+                using var context = new AppDbContext();
+                var types = context.MovieTypes.ToList();
+                var genres = context.Genres.ToList();
+
+                while (true)
+                {
+                    Console.WriteLine("Please choose an option:");
+                    Console.WriteLine("1. To print movie list");
+                    Console.WriteLine("2. To insert a new movie");
+                    Console.WriteLine("3. To exit");
+
+                    var option = Convert.ToInt16(Console.ReadLine());
+
+                    switch (option)
+                    {
+                        case 1:
+                            var movies = context.Movies
+                                .Include(m => m.MovieDetails)
+                                .Include(m => m.MovieType)
+                                .Include(m => m.Genres)
+                                .ToList();
+
+                            Console.WriteLine(JsonSerializer.Serialize(movies, _options));
+
+                            //var movie = movies.FirstOrDefault(m => m.Id == 1);
+                            //var genres = movie?.Genres.Select(g => g.Name).ToList();
+
+                            break;
+                        case 2:
+                            Console.WriteLine("Movie title:");
+                            var title = Console.ReadLine()!;
+
+                            Console.WriteLine("Movie description:");
+                            var description = Console.ReadLine();
+
+                            Console.WriteLine("Release date:");
+                            var releaseDate = Convert.ToDateTime(Console.ReadLine());
+
+                            Console.WriteLine("Rating from 1 to 10:");
+                            var rating = Convert.ToInt16(Console.ReadLine());
+
+                            Console.WriteLine("Movie length:");
+                            var length = Convert.ToInt16(Console.ReadLine());
+
+                            Console.WriteLine("Available types:");
+                            foreach (var item in types)
+                            {
+                                Console.WriteLine($"[{item.Id}] - {item.Name}");
+                            }
+                            var type = Convert.ToInt16(Console.ReadLine());
+
+                            Console.WriteLine("How many genres: ");
+                            var genresCount = Convert.ToInt16(Console.ReadLine());
+                            ICollection<Genre> genresList = [];
+
+                            Console.WriteLine("Available genres:");
+                            foreach (var item in genres)
+                            {
+                                Console.WriteLine($"[{item.Id}] - {item.Name}");
+                            }
+
+                            for (int i = 0; i < genresCount; i++)
+                            {
+                                Console.Write("Genre Id: ");
+                                var genreId = Convert.ToInt16(Console.ReadLine());
+                                var genre = context.Genres.Where(g => g.Id == genreId).ToList();
+                                genresList.Add(genre.FirstOrDefault());
+                            }
+
+                            var movie = new Movie()
+                            {
+                                Title = title,
+                                Description = description,
+                                MovieDetails = new() { ReleaseDate = releaseDate, Length = length, Rating = rating },
+                                MovieTypeId = type,
+                                Genres = genresList
+                            };
+
+                            context.Movies.Add(movie);
+                            context.SaveChanges();
+                            break;
+                        case 3:
+                            return;
+                    }
+                }
+
+                //
+                //int[] selectedGenreIds = [1, 2];
+                //var query = context.Genres.Where(g => selectedGenreIds.Contains(g.Id));
+                //Log.Information(query.ToQueryString());
+                //var genres = query.ToList();
+                //var movie = new Movie()
+                //{
+                //    Title = "TLOTR - second movie!",
+                //    Description = "desc...",
+                //    MovieDetails = new() { ReleaseDate = new DateTime(2025, 1, 1), Length = 180, Rating = 10 },
+                //    MovieTypeId = 1,
+                //    Genres = genres
+                //};
+
+                //var result = context.Movies.Add(movie).Entity;
+                ////context.MovieDetails.Add(new() { MovieId = result.Id, ReleaseDate = new DateTime(2025, 1, 1), Length = 180, Rating = 10 });
+                //context.SaveChanges();
+
+                //Console.WriteLine(JsonSerializer.Serialize(result, _options));
             }
             catch (Exception ex)
             {
--- EFSample.Infrastructure/AppDbContext.cs
+++ EFSample.Infrastructure/AppDbContext.cs
@@ -17,14 +17,32 @@ namespace EFSample.Infrastructure
                 builder.UseSqlServer("Server=localhost;Database=EFMoviesDb;User Id=sa;Password=bitspa.1;TrustServerCertificate=true");
 
             base.OnConfiguring(builder);
-
         }
 
         protected override void OnModelCreating(ModelBuilder builder)
         {
             base.OnModelCreating(builder);
 
+            #region Column Definitions
+
+            //builder.Entity<Movie>().Property(m => m.Id).UseHiLo();
+
+            //builder.Entity<MovieGenre>().HasKey(x => new { x.MovieId, x.GenreId });
+
+            builder.Entity<MovieDetails>(entity =>
+            {
+                entity.HasKey(d => d.MovieId);
+                entity.HasOne(d => d.Movie).WithOne(m => m.MovieDetails).OnDelete(DeleteBehavior.Cascade);
+                entity.Property(d => d.ReleaseDate).HasDefaultValueSql("getdate()");
+            });
+
+            #endregion
+
+            #region Data seeding
+
             builder.SeedData();
+
+            #endregion
         }
     }
 }
